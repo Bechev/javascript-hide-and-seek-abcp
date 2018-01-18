6c1913268848a405d9fd4ef83b7126b1700bbd56
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-18 02:30:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return getFirstSelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const lis =  document.querySelectorAll( '.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i])+n;
+  }
+}
+
+
+function deepestChild(){
+  var main = document.querySelector('grand-node')
+  while(main.children){
+    main = main.children;
+  }
+  return main;
+}
